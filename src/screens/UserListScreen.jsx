c49b06900c994a932cfa3f26f4bf6aa8a931b840
copriot <developer.zeki@gmail.com>
2024-11-28 23:22:06 +0300

    userList screen done and navigate fonk prepared userId:item.id as  props

--- src/screens/UserListScreen.jsx
+++ src/screens/UserListScreen.jsx
@@ -2,9 +2,12 @@ import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React, {useContext} from 'react';
 import {UserContext} from '../context/UserContex';
 import Loader from '../components/Loader';
+import {useNavigation} from '@react-navigation/native';
+import screenNames from '../utils/screenNames';
 const UserListScreen = () => {
   const {users, error, loading} = useContext(UserContext);
   console.warn(users);
+  const navigation = useNavigation();
   return (
     <View style={{flex: 1}}>
       {loading ? (
@@ -13,13 +16,17 @@ const UserListScreen = () => {
         <Text>{error}</Text>
       ) : (
         <FlatList
+          showsVerticalScrollIndicator={false}
           data={users}
           keyExtractor={item => item.id.toString()}
           renderItem={({item}) => (
-            <TouchableOpacity>
-              <View>
-                <Text>{item.name}</Text>
-                <Text>{item.email}</Text>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate(screenNames.USERDETAIL, {userId: item.id})
+              }>
+              <View style={styles.item}>
+                <Text style={styles.title}>{item.name}</Text>
+                <Text style={styles.subtitle}>{item.email}</Text>
               </View>
             </TouchableOpacity>
           )}
@@ -31,4 +38,22 @@ const UserListScreen = () => {
 
 export default UserListScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    backgroundColor: '#fff',
+    marginHorizontal: 16,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {width: 2, height: 1},
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  title: {fontSize: 18, fontWeight: 'bold'},
+  subtitle: {
+    fontSize: 14,
+    color: '#666',
+    marginTop: 5,
+  },
+});
